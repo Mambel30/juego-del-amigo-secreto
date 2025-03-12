2325a531ea72dceef2d3903e231c9573904e1311
Kelvin Mambel <kelvinmambel30@gmail.com>
2025-03-12 00:25:00 -0500

    Se agrego la variable amigo

---
+++ app.js
@@ -0,0 +1,17 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+console.log(amigo);
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value;
+   
+    //console.log(amigo)
+
+    if (!amigo){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigo.pu
+}
